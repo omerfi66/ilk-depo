cfd29e9c989c0c6ddc376b4cc1f74f5042a03b86
omer ipek <omer_furkan_ipek@hotmail.com>
2023-03-31 10:55:29 +0300

    bizim digital

---
+++ bolum5/fonksiyonlar/bizim.dart
@@ -0,0 +1,27 @@
+/* void main(List<String> args) {
+  double x = 5.56, y = 4.56;
+  double ali = carpamaIslemi(x, y);
+  print(ali);
+}
+
+double carpamaIslemi(double a, double b) {
+  double toplam = a * b;
+  return toplam;
+}
+ */
+
+void main(List<String> args) {
+  String ad = "ömer", soyad = " ipek";
+  String toplam = Isim_ve_soyisim(ad, soyad);
+  print(toplam);
+}
+
+String Isim_ve_soyisim(String a, String b) {
+  return a + b;
+}
+
+
+
+
+
+//camelCase   snake_case
